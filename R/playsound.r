9e9e6623a3da0f3fd284b228c65c4d9641ed7450
Wolfgang Viechtbauer <wvb@wvbauer.com>
2024-12-17 14:06:31 +0100

    Correct handling of volume in playsound().

--- R/playsound.r
+++ R/playsound.r
@@ -4,21 +4,24 @@ playsound <- function(sound, volume) {
    volume[volume > 1] <- 1
 
    if (Sys.info()[["sysname"]] == "Linux") {
-      volume <- round(volume * 65536)
-      if (volume > 0)
+      if (volume > 0) {
+         volume <- round(volume * 65536)
          system(paste0("paplay --volume=", volume, " ", sound), ignore.stdout=TRUE, ignore.stderr=TRUE, wait=FALSE)
+      }
    }
 
    if (Sys.info()[["sysname"]] == "Darwin") {
-      sound <- sub(".ogg", ".mp3", sound, fixed=TRUE)
-      if (volume > 0)
+      if (volume > 0) {
+         sound <- sub(".ogg", ".mp3", sound, fixed=TRUE)
          system(paste0("afplay -v ", volume, " ", sound), ignore.stdout=TRUE, ignore.stderr=TRUE, wait=FALSE)
+      }
    }
 
    if (Sys.info()[["sysname"]] == "Windows") {
-      sound <- sub(".ogg", ".mp3", sound, fixed=TRUE)
-      system(paste0("mpg123 ", sound), ignore.stdout=TRUE, ignore.stderr=TRUE, wait=FALSE)
-      #powershell -c (New-Object Media.SoundPlayer "C:\Windows\Media\notify.wav").PlaySync();
+      if (volume > 0) {
+         sound <- sub(".ogg", ".mp3", sound, fixed=TRUE)
+         system(paste0("mpg123 ", sound), ignore.stdout=TRUE, ignore.stderr=TRUE, wait=FALSE)
+      }
    }
 
    invisible()
