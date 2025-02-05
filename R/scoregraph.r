fbb699726c77fbf702f5570b3b98a37ea2ee9e63
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-02-05 13:22:16 +0100

    Adjusted colors in .scoregraph().

--- R/scoregraph.r
+++ R/scoregraph.r
@@ -4,10 +4,16 @@
 
    par(new=TRUE, mar=rep(11,4))
 
-   plot(x$played, x$score, type="o", pch=19, lwd=2,
-        xlab="", ylab="Score", ylim=c(0,100), bty="l", las=1,
-        col=.get("col.fg"), col.axis=.get("col.fg"), col.lab=.get("col.fg"), xaxt="n")
-   axis(side=1, at=x$played, col.axis=.get("col.fg"))
+   plot(NA, xlim=range(x$played), ylim=c(0,100), xlab="", ylab="Score",
+        bty="l", las=1, col.axis=.get("col.top"), col.lab=.get("col.top"), xaxt="n")
+   axis(side=1, at=x$played, col.axis=.get("col.top"))
+
+   #xpos <- axTicks(side=1)
+   #segments(xpos, par("usr")[3], xpos, par("usr")[4], lty="dotted", col=.get("col.fg"))
+   #ypos <- axTicks(side=2)
+   #segments(par("usr")[1], ypos, par("usr")[2], ypos, lty="dotted", col=.get("col.fg"))
+
+   points(x$played, x$score, type="o", pch=21, lwd=2, col=.get("col.square.l"), bg=.get("col.square.d"))
 
    par(new=FALSE, mar=rep(5.2,4))
 
