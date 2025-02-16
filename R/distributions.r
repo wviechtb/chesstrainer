f8cf2210759de3c28afea76d9ac960d0a59d8850
Wolfgang Viechtbauer <wvb@wvbauer.com>
2025-02-16 12:02:21 +0100

    Slight improvements to distribution plots.

--- R/distributions.r
+++ R/distributions.r
@@ -3,22 +3,29 @@
    if (length(scores) == 1L)
       return(invisible())
 
-   par(mfrow=c(2,2))
+   par(mfrow=c(2,2), pty="m")
 
    hist(scores, breaks="FD", las=1, col.axis=.get("col.top"), col.lab=.get("col.top"), col.main=.get("col.fg"),
-        xlab=.text("score"), col=.get("col.square.d"), border=.get("col.square.l"), main=paste0("Histogram: ", .text("score")))
+        xlab=.text("score"), col=.get("col.square.d"), border=.get("col.square.l"), main=paste0("Histogram: ", .text("score")), xlim=c(0,100))
+   #box(which="figure", col=.get("col.help.border"), lwd=lwd)
    hist(played, breaks="FD", las=1, col.axis=.get("col.top"), col.lab=.get("col.top"), col.main=.get("col.fg"),
         xlab=.text("played"), col=.get("col.square.d"), border=.get("col.square.l"), main=paste0("Histogram: ", .text("played")))
+   #box(which="figure", col=.get("col.help.border"), lwd=lwd)
    if (length(c(na.omit(dayslp))) >= 2L) {
       hist(dayslp, breaks="FD", las=1, col.axis=.get("col.top"), col.lab=.get("col.top"), col.main=.get("col.fg"),
            xlab=.text("days"), col=.get("col.square.d"), border=.get("col.square.l"), main=paste0("Histogram: ", .text("days")))
    } else {
       plot(NA, xlim=c(0,1), ylim=c(0,1), xlab="", ylab="", xaxt="n", yaxt="n", bty="n")
    }
+   #box(which="figure", col=.get("col.help.border"), lwd=lwd)
    plot(played, scores, las=1, col.axis=.get("col.top"), col.lab=.get("col.top"), col=.get("col.square.l"), col.main=.get("col.fg"),
-        pch=21, bg=.get("col.square.d"), bty="l", main=paste0(.text("played"), " vs. ", .text("score")), xlab=.text("played"), ylab=.text("score"))
+        pch=21, bg=.get("col.square.d"), bty="l", main=paste0(.text("played"), " vs. ", .text("score")), xlab=.text("played"), ylab=.text("score"),
+        ylim=c(0,100))
+   #box(which="figure", col=.get("col.help.border"), lwd=lwd)
 
-   par(mfrow=c(1,1))
+   par(mfrow=c(1,1), pty="s")
+
+   box(which="figure", col=.get("col.help.border"), lwd=lwd+3)
 
    getGraphicsEvent(prompt="Chesstrainer", consolePrompt="", onMouseDown=function(button, x, y) return(""), onKeybd=function(key) return(""))
 
